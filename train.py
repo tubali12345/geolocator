7126712e32123e641ea66cc381b72eefa3f72646
tubali12345 <72875354+tubali12345@users.noreply.github.com>
2022-12-28 12:47:37 +0100

    added RESNET50 train

--- train.py
+++ train.py
@@ -11,13 +11,12 @@ path = 'C:/Users/TuriB/Documents/5.felev/bevadat/geo_project/'
 
 def callbacks(filepath: str, model_type: str) -> list:
     logdir = f'{path}logs/{model_type}_{date.today()}'
-    # train_writer = tf.summary.create_file_writer(f'{logdir}/batch_level') #batch-level logging, need to add
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_weights_only=True,
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_weights_only=True,
                                  save_best_only=True, mode='auto')
     tensor_board = TensorBoard(log_dir=logdir)
-    early_stop = EarlyStopping(monitor='val_accuracy', patience=1)
+    early_stop = EarlyStopping(monitor='val_accuracy', patience=5, verbose=1)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
-                                  patience=4, min_lr=0.0005)
+                                  patience=1, min_lr=0)
     return [checkpoint, tensor_board, early_stop, reduce_lr]
 
 
@@ -25,10 +24,13 @@ def compile_SGD(model, lr=1e-4, momentum=0.9):
     return model.compile(optimizer=SGD(learning_rate=lr, momentum=momentum), loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-def train(model, train_data: tf.data.Dataset, val_data: tf.data.Dataset, num_epochs: int, heading: int, model_name: str):
+def train(model, train_data: tf.data.Dataset, val_data: tf.data.Dataset, num_epochs: int, model_name: str, heading=None):
     outdir = Path(f'{path}weights/{model_name}_{date.today()}')
     outdir.mkdir(exist_ok=True, parents=True)
-    filepath = f'{path}weights/{model_name}_{date.today()}/{heading}' + '.{epoch:02d}-{loss:.2f}.hdf5'
+    if heading is not None:
+        filepath = f'{path}weights/{model_name}_{date.today()}/{heading}' + '.{epoch:02d}-{loss:.2f}.hdf5'
+    else:
+        filepath = f'{path}weights/{model_name}_{date.today()}/' + '.{epoch:02d}-{loss:.2f}.hdf5'
     return model.fit(train_data,  validation_data=val_data, epochs=num_epochs, callbacks=callbacks(filepath, model_name))
 
 
@@ -45,6 +47,16 @@ def train_ensemble(create_model_func, model_name: str, num_epochs=50) -> dict:
     return histories
 
 
+def train_resnet50(model_name):
+    model = RESNET50()
+    compile_SGD(model)
+    data = Data(validation_split=0.15, image_size=(256, 256), batch_size=8, seed=123)
+    train_data = data.load_train('data')
+    val_data = data.load_val('data')
+    return train(model, train_data, val_data, num_epochs=50, model_name=model_name)
+
+
 if __name__ == '__main__':
-    train_ensemble(RESNET50, 'resnet50')
+    train_resnet50('1RESNET50')
+    # train_ensemble(RESNET50, 'resnet50 ensemble')
     # train_ensemble(RESNET101, 'resnet101')
